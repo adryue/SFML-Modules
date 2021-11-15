e1dbf6aa2f0eaa6fc97ad5ddb8090235d945e047
Adrian Y <66759189+adryue@users.noreply.github.com>
2021-11-15 00:09:14 -0800

    Added more functions and variables in EnergyBar class
    Deleted: 
    level variable, 
    setMaxEnergy
    
    Added: 
    maxSize variable,
    addEnergy, 
    addPercent, 
    getMaxEnergy, 
    getEnergy, 
    getPercent
    
    Edited: 
    constructor, 
     setCurrentEnergy (renamed to setEnergy), 
    setPercent

--- EnergyBar.cpp
+++ EnergyBar.cpp
@@ -1,19 +1,50 @@
-#include <EnergyBar.h>
 #include <SFML/Graphics.hpp>
+#include "EnergyBar.h"
 
-bar_t::bar_t(sf::Vector2f barSize, sf::Color::Color color)
+bar_t::bar_t(sf::Vector2f barSize, sf::Color color, int capacity)
 {
-    setFillColor()
+    setSize(barSize);
+    setFillColor(color);
+    maxSize = barSize.x;
+    maxEnergy = capacity;
+    currentEnergy = capacity;
+    percent = 100.0;
 }
-void setMaxEnergy(int energyAmount)
+void bar_t::setEnergy(float energyAmount)
 {
-
+    currentEnergy = energyAmount;
+    percent = (currentEnergy / maxEnergy) * 100;
+    setSize(sf::Vector2f(maxSize * (currentEnergy / maxEnergy), getSize().y));
 }
-void setCurrentEnergy(int energyAmount)
+void bar_t::setPercent(float newPercent)
 {
-
+    percent = newPercent;
+    currentEnergy = maxEnergy * (percent / 100);
+    setSize(sf::Vector2f(maxSize * (currentEnergy / maxEnergy), getSize().y));
 }
-void setPercent(int percent)
-{
 
+void bar_t::addEnergy(float energyAmount)
+{
+    currentEnergy += energyAmount;
+    percent = (currentEnergy / maxEnergy) * 100;
+    setSize(sf::Vector2f(maxSize * (currentEnergy / maxEnergy), getSize().y));
+}
+void bar_t::addPercent(float addPercent)
+{
+    percent += addPercent;
+    currentEnergy = maxEnergy * (percent / 100);
+    setSize(sf::Vector2f(maxSize * (currentEnergy / maxEnergy), getSize().y));
+}
+
+float bar_t::getMaxEnergy()
+{
+    return maxEnergy;
+}
+float bar_t::getEnergy()
+{
+    return currentEnergy;
+}
+float bar_t::getPercent()
+{
+    return percent;
 }
--- EnergyBar.h
+++ EnergyBar.h
@@ -1,18 +1,29 @@
 #ifndef ENERGYBAR_H_INCLUDED
 #define ENERGYBAR_H_INCLUDED
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
+
 class bar_t : public sf::RectangleShape
 {
 private:
-    int level;
-    int maxEnergy;
-    int currentEnergy;
+    float maxSize;
+    float maxEnergy;
+    float currentEnergy;
+    float percent;
 public:
-    bar_t(sf::Vector2f barSize, sf::Color::Color color);
+    bar_t(sf::Vector2f barSize, sf::Color color, int capacity);
 
-    void setMaxEnergy(int energyAmount);
-    void setCurrentEnergy(int energyAmount);
-    void setPercent(int percent);
+    //void setMaxEnergy(int energyAmount);
+    void setEnergy(float energyAmount);
+    void setPercent(float newPercent);
+
+    void addEnergy(float energyAmount);
+    void addPercent(float addPercent);
+
+    float getMaxEnergy();
+    float getEnergy();
+    float getPercent();
 };
 
 #endif // ENERGYBAR_H_INCLUDED
